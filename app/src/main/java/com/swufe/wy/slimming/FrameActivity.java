e192e9850bc6a25639b74f765da387f2fa17cb1d
Your Name <913105698@qq.com>
2019-06-09 16:03:03 +0800

    新增了对底部RadioButton组的美化及点击时响应的不同效果。

--- app/src/main/java/com/swufe/wy/slimming/FrameActivity.java
+++ app/src/main/java/com/swufe/wy/slimming/FrameActivity.java
@@ -33,15 +33,16 @@ public class FrameActivity extends FragmentActivity {
         mFragments[0] = fragmentManager.findFragmentById(R.id.fragment_home);
         mFragments[1] = fragmentManager.findFragmentById(R.id.fragment_plan);
         mFragments[2] = fragmentManager.findFragmentById(R.id.fragment_hot);
+        //控制fragment的隐藏和显示
+        fragmentTransaction = fragmentManager.beginTransaction().hide(mFragments[0]).hide(mFragments[1]).hide(mFragments[2]);
+        fragmentTransaction.show(mFragments[0]).commit();
 
         //获取三个元素
         rbtHome= findViewById(R.id.radioHome);
         rbtPlan = (RadioButton)findViewById(R.id.radioPlan);
         rbtHot = (RadioButton)findViewById(R.id.radioHot);
+        rbtHome.setBackgroundResource(R.drawable.shape);
 
-        //控制fragment的隐藏和显示
-        fragmentTransaction = fragmentManager.beginTransaction().hide(mFragments[0]).hide(mFragments[1]).hide(mFragments[2]);
-        fragmentTransaction.show(mFragments[0]).commit();
 
         //设置按钮切换
         radioGroup = (RadioGroup) findViewById(R.id.bottomGroup);
@@ -52,15 +53,23 @@ public class FrameActivity extends FragmentActivity {
                 Log.i("radioGroup", "checkId=" + checkedId);
                 fragmentTransaction = fragmentManager.beginTransaction().hide(mFragments[0]).hide(mFragments[1]).hide(mFragments[2]);
 
+                //修改背景颜色
+                rbtHome.setBackgroundResource(R.drawable.shape1);
+                rbtPlan.setBackgroundResource(R.drawable.shape1);
+                rbtHot.setBackgroundResource(R.drawable.shape1);
+
                 switch (checkedId) {
                     case R.id.radioHome:
                         fragmentTransaction.show(mFragments[0]).commit();
+                        rbtHome.setBackgroundResource(R.drawable.shape);
                         break;
                     case R.id.radioPlan:
                         fragmentTransaction.show(mFragments[1]).commit();
+                        rbtPlan.setBackgroundResource(R.drawable.shape);
                         break;
                     case R.id.radioHot:
                         fragmentTransaction.show(mFragments[2]).commit();
+                        rbtHot.setBackgroundResource(R.drawable.shape);
                         break;
                     default:
                         break;
